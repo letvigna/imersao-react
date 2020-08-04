d2fbddeb2aabe941718630a278fe956d4f3abdd5
Letícia Vigna <leticia.vigna@hotmail.com>
2020-08-04 13:41:35 -0300

    Inserção dos componentes prontos da Home

--- letflix/src/App.js
+++ letflix/src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+import dadosIniciais from './data/dados_iniciais.json';
 
 function App() {
   return (
     <div>
       <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Descrição aqui"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Footer />
     </div>
   );
 }
