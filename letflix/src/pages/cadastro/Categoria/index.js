714f0b08f6def94ba0f98950bdb6e364b29f6a80
Letícia Vigna <leticia.vigna@hotmail.com>
2020-08-05 19:27:58 -0300

    Cadastro de categorias

--- letflix/src/pages/cadastro/Categoria/index.js
+++ letflix/src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import DefaultTemplate from '../../../components/DefaultTemplate';
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState([]);
+  const [nomeCategoria, setNomeCategoria] = useState('');
+
   return (
     <DefaultTemplate>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {nomeCategoria}</h1>
+      <form onSubmit={event => {
+        event.preventDefault();
+        setCategorias([
+          ...categorias,
+          nomeCategoria
+        ]);
+      }}>
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+            onChange={event => {
+              setNomeCategoria(event.target.value);
+            }}
+          />
+        </label>
+        <button>
+          Cadastrar
+        </button>
+      </form>
       <Link to="/">
         Ir para home
       </Link>
