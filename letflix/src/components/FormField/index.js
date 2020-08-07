3d120d0d104ed25ad3d68284ce91b5661f5423a4
Letícia Vigna <leticia.vigna@hotmail.com>
2020-08-06 23:49:25 -0300

    Otimização do FormField

--- letflix/src/components/FormField/index.js
+++ letflix/src/components/FormField/index.js
@@ -3,14 +3,13 @@ import React from "react";
 function FormField({ label, type, name, value, onChange }) {
   return (
     <div>
-      <label>
-        {label}: <input
-          type={type}
-          value={value}
-          name={name}
-          onChange={onChange}
-        />
-      </label>
+      <label>{label}: </label>
+      <input
+        type={type}
+        value={value}
+        name={name}
+        onChange={onChange}
+      />
     </div>
   );
 }
