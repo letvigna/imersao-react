55bae56fba57750087039562cbbd0dc988bb4855
Letícia Vigna <leticia.vigna@hotmail.com>
2020-08-06 23:31:52 -0300

    Funcionalidade do formulário de categoria

--- letflix/src/pages/cadastro/Categoria/index.js
+++ letflix/src/pages/cadastro/Categoria/index.js
@@ -1,34 +1,93 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import DefaultTemplate from '../../../components/DefaultTemplate';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+  const categoria = {
+    nome: '',
+    descricao: '',
+    cor: ''
+  };
+  const [novaCategoria, setNovaCategoria] = useState(categoria);
   const [categorias, setCategorias] = useState([]);
-  const [nomeCategoria, setNomeCategoria] = useState('');
+
+  const handleChange = event => {
+    /*
+      Cria variáveis com o mesmo nome do atributo desejado:
+        → getAttribute = event.target.getAttribute()
+        → value = event.target.value
+
+    const { getAttribute, value } = event.target;
+    setNovaCategoria({
+      ...novaCategoria,
+      [getAttribute('name')]: value,
+    });
+    */
+
+    const campo = event.target.getAttribute('name');
+    const valor = event.target.value;
+
+    setNovaCategoria({
+      ...novaCategoria,
+      [campo]: valor,
+    });
+  }
 
   return (
     <DefaultTemplate>
-      <h1>Cadastro de Categoria: {nomeCategoria}</h1>
+      <h1>Cadastro de Categoria: {novaCategoria.nome}</h1>
       <form onSubmit={event => {
         event.preventDefault();
+
+        // Adiciona a nova categoria ao array de categorias
         setCategorias([
           ...categorias,
-          nomeCategoria
+          novaCategoria
         ]);
+
+        // "Limpa" o formulário com os valores default
+        setNovaCategoria(categoria);
       }}>
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-            onChange={event => {
-              setNomeCategoria(event.target.value);
-            }}
-          />
-        </label>
+        <FormField
+          label="Nome"
+          name="nome"
+          type="text"
+          value={novaCategoria.nome}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Descrição"
+          name="descricao"
+          type="text"
+          value={novaCategoria.descricao}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Cor"
+          name="cor"
+          type="color"
+          value={novaCategoria.cor}
+          onChange={handleChange}
+        />
+
+        <br />
         <button>
           Cadastrar
         </button>
       </form>
+      {/*
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          )
+        })}
+      </ul>
+      */}
+      <br />
       <Link to="/">
         Ir para home
       </Link>
